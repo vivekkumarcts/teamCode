95297c2ef6ef7f719e39cd834509910838a9c73a
vivekkumarcts <Vivek.Kumar9@cognizant.com>
2014-07-25 17:23:53 +0530

    First level code
    Flow to be corrected

---
+++ Vivek-app/controllers/ListView.js
@@ -0,0 +1,11 @@
+function Tasks($scope)
+{
+    $scope.Tasks = [
+        {type:'Earthquake',reported:'05/01/2014',time:'6:36 PM',duration:'3hrs ago', ImgUrl: 'Icon_Active_Crisis.svg',crisisData:'8.2',navigation:'detailView.html'},
+        {type:'Earthquake',reported:'05/01/2014',time:'6:36 PM',duration:'3hrs ago', ImgUrl: 'Icon_Active_Crisis.svg',crisisData:'5.2',navigation:'detailView.html'},
+        {type:'Earthquake',reported:'05/01/2014',time:'6:36 PM',duration:'3hrs ago', ImgUrl: 'Icon_Active_Crisis.svg',crisisData:'3.2',navigation:'detailView.html'}
+    ];
+    $scope.getNavigation = function(task) {
+        location.href=task.navigation;
+    };
+}
---
+++ Vivek-app/controllers/Tasks.js
@@ -0,0 +1,13 @@
+function Tasks($scope)
+{
+    $scope.Tasks = [
+        {name: 'Active Crisis Info', ImgUrl: '../images/Icon_Active_Crisis.svg',navigation:'listView.html'},
+        {name: 'Respond to a Crisis', ImgUrl: '../images/Icon_RespondtoCrisis.png',navigation:'home.html'},
+        {name: 'CM Policies and Procedures', ImgUrl: '../images/Icon_RespondtoCrisis.png',navigation:'home.html'},
+        {name: 'View Wallet Card', ImgUrl: '../images/Icon_WalletCard.png',navigation:'home.html'}];
+
+    $scope.getNavigation = function(task) {
+        location.href=task.navigation;
+    };
+}
+
